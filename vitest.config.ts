c9faf86bc70fc3143c3ed9c1e37fcb0c4aeb97ae
Vladimir Volek <volek.vladimir@gmail.com>
2022-11-26 16:48:35 +0100

    fix: coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,10 @@ export default defineConfig({
   test: {
     include: ['./test/tests/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     coverage: {
-      statements: 93.02,
+      statements: 93,
       branches: 79.24,
       functions: 91.66,
-      lines: 93.02,
+      lines: 93,
       exclude: ['test', 'src/index.ts'],
     },
   },

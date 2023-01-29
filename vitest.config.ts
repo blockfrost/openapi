4428e5b693fc260f99e9d9aedba32050cdf36657
slowbackspace <slowbackspace@gmail.com>
2023-01-30 00:45:26 +0100

    fix: cip25v2 metadata validation

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,10 @@ export default defineConfig({
   test: {
     include: ['./test/tests/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     coverage: {
-      statements: 93,
+      statements: 90,
       branches: 79.24,
-      functions: 91.66,
-      lines: 93,
+      functions: 87,
+      lines: 90,
       exclude: ['test', 'src/index.ts'],
     },
   },

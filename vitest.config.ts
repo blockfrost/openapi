f3fdc63e496ce51ced5bd515bce644919d405ce1
slowbackspace <slowbackspace@gmail.com>
2023-02-02 16:31:50 +0100

    fix(tests): add missing cbor in fixture

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,10 @@ export default defineConfig({
   test: {
     include: ['./test/tests/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     coverage: {
-      statements: 90,
-      branches: 79.24,
+      statements: 91,
+      branches: 78,
       functions: 87,
-      lines: 90,
+      lines: 91,
       exclude: ['test', 'src/index.ts'],
     },
   },

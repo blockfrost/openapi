514b8cba9990a12ade90f7940c956ecff4afaedc
Vladimir Volek <volek.vladimir@gmail.com>
2023-08-30 13:15:57 +0200

    chore: first redirect

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 module.exports = {
   async redirects() {
     return [
+      // root
+      {
+        source: '/',
+        destination:
+          'http://www.blockfrost.dev/api/blockfrost-io-api-documentation',
+        permanent: true,
+      },
+      // addresses
       {
         source:
-          '#tag/Cardano-Accounts/paths/~1accounts~1%7Bstake_address%7D/get',
-        destination: 'https:/blockfrost.dev/api/specific-account-address',
+          '/#tag/Cardano-Accounts/paths/~1accounts~1%7Bstake_address%7D/get',
+        destination: 'http://www.blockfrost.dev/api/specific-account-address',
         permanent: true,
       },
     ];

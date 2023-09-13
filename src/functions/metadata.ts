bc70331786e7bf1788ca38f7cfbc81590bd98d97
leo42 <leantrosh@gmail.com>
2023-09-14 00:11:53 +0200

    Support HexString metadata (not including '0x')

--- src/functions/metadata.ts
+++ src/functions/metadata.ts
@@ -147,23 +147,32 @@ export const getOnchainMetadata = (
         onchainMetadataCbor,
       );
       const foundMetadata = foundAssetInCbor
-        ? internalOnchainMetadata[policyIdVersion2][assetNameVersion2]
+        ? internalOnchainMetadata[policyIdVersion2][assetNameVersion2] 
         : null;
       if (foundMetadata) {
         onchainMetadataResult = foundMetadata;
         isFound = true;
       } else {
-        // Fallback
-        // Due to previously missing and then incorrect implementation, metadata submitted as v2
-        // (i.e. "version" field encoded in CBOR was set to 2) with mistakenly text-encoded
-        // policies/asset names (which is CIP25v1 method of encoding) instead of correct CIP25v2
-        // byte-encoded keys passed as valid. (Note that the version number is a user input
-        // independent of the actual metadata content).
-        // To prevent breaking change, in case we don't find metadata for an asset under
-        // correct byte-encoded key, we fallback to CIP25v1 text-encoded format.
-        onchainMetadataResult =
-          internalOnchainMetadata[policyId][assetNameVersion1] || null;
-        isFound = false;
+        
+        const foundHexStringMetadata = foundAssetInCbor
+          ? internalOnchainMetadata[assetNameBase][policyId] 
+          : null;
+          if (foundHexStringMetadata) {
+            onchainMetadataResult = foundHexStringMetadata;
+            isFound = true;
+          }else{
+            // Fallback
+            // Due to previously missing and then incorrect implementation, metadata submitted as v2
+            // (i.e. "version" field encoded in CBOR was set to 2) with mistakenly text-encoded
+            // policies/asset names (which is CIP25v1 method of encoding) instead of correct CIP25v2
+            // byte-encoded keys passed as valid. (Note that the version number is a user input
+            // independent of the actual metadata content).
+            // To prevent breaking change, in case we don't find metadata for an asset under
+            // correct byte-encoded key, we fallback to CIP25v1 text-encoded format.
+            onchainMetadataResult =
+              internalOnchainMetadata[policyId][assetNameVersion1] || null;
+            isFound = false;
+        }
       }
     } catch (error) {
       onchainMetadataResult = null;

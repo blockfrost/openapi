736e5d40ba8ae6947137cfe9fbe6cadaa98e79a5
Vladimir Volek <volek.vladimir@gmail.com>
2023-11-07 22:54:58 +0100

    fix: metadata schema in rust models

---
+++ rust/CHNAGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+## [Unreleased]
+
+## [0.0.2] - 2023-07-11
+
+### Fixed
+
+- json_metadata schema
+
+## [0.0.1] - 2023-07-06
+
+### Added
+
+- initial commit
--- rust/build.rs
+++ rust/build.rs
@@ -1,30 +1,30 @@
-use std::env;
-use std::fs;
-use std::path::Path;
+// use std::env;
+// use std::fs;
+// use std::path::Path;
 
 fn main() {
-    let out_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
-    let model_path = Path::new("src/models");
-    let entries = fs::read_dir(model_path).unwrap();
+    // let out_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    // let model_path = Path::new("src/models");
+    // let entries = fs::read_dir(model_path).unwrap();
 
-    let mut content = String::new();
+    // let mut content = String::new();
 
-    for entry in entries {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        let file_stem = path.file_stem().unwrap();
+    // for entry in entries {
+    //     let entry = entry.unwrap();
+    //     let path = entry.path();
+    //     let file_stem = path.file_stem().unwrap();
 
-        if let Some(file_name_str) = file_stem.to_str() {
-            if file_name_str == "mod" {
-                continue;
-            }
-            content.push_str(&format!("pub mod {};\n", file_name_str));
-        } else {
-            panic!("File name is not valid Unicode: {:?}", file_stem);
-        }
-    }
+    //     if let Some(file_name_str) = file_stem.to_str() {
+    //         if file_name_str == "mod" {
+    //             continue;
+    //         }
+    //         content.push_str(&format!("pub mod {};\n", file_name_str));
+    //     } else {
+    //         panic!("File name is not valid Unicode: {:?}", file_stem);
+    //     }
+    // }
 
-    let dest_path = Path::new(&out_dir).join("./src/models/mod.rs");
+    // let dest_path = Path::new(&out_dir).join("./src/models/mod.rs");
 
-    fs::write(dest_path, content).unwrap();
+    // fs::write(dest_path, content).unwrap();
 }

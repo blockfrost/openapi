2d03bd49c4a436df0481cd202fae5e22df650f00
1000101 <b1000101@pm.me>
2023-12-17 15:04:23 +0100

    fix: typos

--- src/generated-types.ts
+++ src/generated-types.ts
@@ -492,7 +492,7 @@ export interface paths {
   };
   '/governance/dreps/{drep_id}/delegators': {
     /**
-     * DRep pool delegators
+     * DRep delegators
      * @description List of Drep delegators.
      */
     get: {
@@ -534,7 +534,7 @@ export interface paths {
   '/governance/dreps/{drep_id}/metadata': {
     /**
      * DRep metadata
-     * @description DRep information.
+     * @description DRep metadata information.
      */
     get: {
       parameters: {

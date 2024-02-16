66dd682b59b2cc2ad268cee74f61c8f9d0508fcd
slowbackspace <slowbackspace@gmail.com>
2024-02-16 09:40:35 +0100

    chore: allow CIP68v2

--- src/types/metadata.ts
+++ src/types/metadata.ts
@@ -9,38 +9,40 @@ export type GetOnchainMetadataResult = {
 };
 
 export type CIPTypes = 'CIP25v1' | 'CIP25v2' | null;
-export type CIP68Version = 'CIP68v1' | null;
+export type ValidCIP68Version = 'CIP68v1' | 'CIP68v2';
+export type CIP68Version = ValidCIP68Version | null;
 
 export type validateCIP68MetadataInput = {
   metadata: unknown;
   version: number;
+  extra: string | undefined;
 } | null;
 
 export type validateCIP68MetadataOverload = {
   (input: validateCIP68MetadataInput, type: 'ft'):
     | {
-        version: 'CIP68v1';
+        version: ValidCIP68Version;
         extra: string | undefined;
         metadata: Schemas['onchain_metadata_cip68_ft_333'];
       }
     | false;
   (input: validateCIP68MetadataInput, type: 'nft'):
     | {
-        version: 'CIP68v1';
+        version: ValidCIP68Version;
         extra: string | undefined;
         metadata: Schemas['onchain_metadata_cip68_nft_222'];
       }
     | false;
   (input: validateCIP68MetadataInput, type: 'rft'):
     | {
-        version: 'CIP68v1';
+        version: ValidCIP68Version;
         extra: string | undefined;
         metadata: Schemas['onchain_metadata_cip68_rft_444'];
       }
     | false;
   (input: validateCIP68MetadataInput, type: 'nft' | 'ft' | 'rft'):
     | {
-        version: 'CIP68v1';
+        version: ValidCIP68Version;
         extra: string | undefined;
         metadata:
           | Schemas['onchain_metadata_cip68_nft_222']

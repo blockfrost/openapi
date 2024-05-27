b164cd34e6c1d3da4dc3059aeedb842726688559
Vladimir Volek <volek.vladimir@gmail.com>
2024-05-27 15:51:41 +0200

    feat: new docs

--- src/generated-types.ts
+++ src/generated-types.ts
@@ -5277,7 +5277,7 @@ export interface components {
         /** @description Content of the JSON metadata */
         json_metadata: string | {
           [key: string]: unknown;
-        } | unknown[] | number | boolean | null;
+        } | unknown[] | number | boolean;
       })[];
     /**
      * @example [
@@ -6208,7 +6208,7 @@ export interface components {
       /** @description JSON contents of the `timelock` script, null for `plutus` scripts */
       json: string | {
         [key: string]: unknown;
-      } | unknown[] | number | boolean | null;
+      } | unknown[] | number | boolean;
     };
     /**
      * @example {

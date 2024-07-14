a7c18fc5cf82e1694662649e13f2eebd6060512f
1000101 <b1000101@pm.me>
2024-07-14 20:31:55 +0200

    feat: CIP-1694

--- shell.nix
+++ shell.nix
@@ -10,8 +10,9 @@ stdenv.mkDerivation {
     export PATH="$PATH:$(pwd)/node_modules/.bin"
     yarn
     echo "Rebuilding docs"
+    yarn build
     yarn run bundle && yarn run generate-docs && yarn run generate-types
-    echo -e "\nDON'T FORGET TO RUN THIS BEFORE OPENING PR:"
-    echo "yarn run bundle && yarn run generate-docs && yarn run generate-types"
+    echo -e "\nDON'T FORGET TO RUN yarn build && yarn run bundle && yarn run generate-docs && yarn run generate-types"
+    echo "yarn build && yarn run bundle && yarn run generate-docs && yarn run generate-types"
   '';
 }
